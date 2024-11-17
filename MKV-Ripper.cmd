1543abc4d38c1d08f4ff38c8f3740e87f48c4945
ScriptTiger <thescripttiger@gmail.com>
2024-11-17 21:13:34 +0800

    Update MKV-Ripper.cmd
    - Added remarks
    - Added error message for unsupported codecs
    - Simplified enumeration

--- MKV-Ripper.cmd
+++ MKV-Ripper.cmd
@@ -9,19 +9,37 @@ rem =====
 
 setlocal ENABLEDELAYEDEXPANSION
 
+rem Generic call to FFmpeg
 set FFMPEG=call "%~dp0bin\ffmpeg.exe" -hide_banner -i %1
 
+rem Enumerate video and audio streams, and generate associated output arguments
 set OUTPUT=
 set i=0
 for /f tokens^=4^,5^ delims^=^:^,^_^(^  %%0 in ('%FFMPEG% -vn -an -dn -sn -f null "" 2^>^&1^| findstr Stream') do (
+	set EXT=
 	if "%%0" == "Video" set OUTPUT=!OUTPUT! -map 0:!i! -c:v copy -an -dn -sn 
 	if "%%0" == "Audio" set OUTPUT=!OUTPUT! -map 0:!i! -c:a copy -vn -dn -sn 
-	if "%%1" == "h264" set OUTPUT=!OUTPUT! "%~dpn1-!i!.mp4"
-	if "%%1" == "hevc" set OUTPUT=!OUTPUT! "%~dpn1-!i!.mp4"
-	if "%%1" == "pcm" set OUTPUT=!OUTPUT! "%~dpn1-!i!.wav"
-	if "%%1" == "aac" set OUTPUT=!OUTPUT! "%~dpn1-!i!.m4a"
+	if "%%1" == "h264" set EXT=mp4
+	if "%%1" == "hevc" set EXT=mp4
+	if "%%1" == "pcm" set EXT=wav
+	if "%%1" == "aac" set EXT=m4a
+	if "!EXT!" == "" (
+		call :Unsupported %%0 %%1
+		goto Exit
+	)
+	set OUTPUT=!OUTPUT! "%~dpn1-!i!.!EXT!"
 	set /a i=i+1
 )
 
+rem Output streams to files
 %FFMPEG% %OUTPUT%
+
+rem Pause and exit
+:Exit
 pause
+exit /b
+
+rem Display error message on unsupported codec
+:Unsupported
+echo The %2 %1 codec is currently unsupported. Please submit a PR or issue on GitHub to request support to be added.
+exit /b
